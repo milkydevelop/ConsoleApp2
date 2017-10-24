89fe60088c39f951f86106c47d80aaea72013e87
milkydevelop <33061921+milkydevelop@users.noreply.github.com>
2017-10-25 00:32:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Random liczba = new Random();
+            double[] tablica = new double[100];
+
+            for (i=0; i < tablica.Length; i++)
+            {
+                tablica[i] = liczba.NextDouble();
+            }
+            var query = tablica.Select(n => n).ToArray();
+            foreach (var item in query)
+            {
+                Console.WriteLine(item + ", ");
+            }
+
+
+        }
+    }
+}
